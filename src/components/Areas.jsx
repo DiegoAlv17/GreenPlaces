54f0e5ec4eb0320028a6205bc8d4bf33fc461ad8
unknown <diegonapa17@gmail.com>
2024-11-20 03:10:45 -0500

    logo

--- src/components/Areas.jsx
+++ src/components/Areas.jsx
@@ -126,4 +126,4 @@ const Areas = () => {
 };
 
 export default Areas;
->>>>>>> lorena
+
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,22 @@
-
 const Hero = () => {
   return (
-    <div id="inicio" className="relative bg-black w-full h-screen max-sm:h-auto">
-      <div className="absolute inset-0 w-full h-screen max-sm:h-auto overflow-x-hidden" >
-        <video 
+    <div id="inicio" className="relative bg-black w-screen h-screen">
+      <div className="absolute inset-0 w-screen h-screen">
+        <video
           src="/video/fondo.mp4"
-          className="w-full h-full object-cover opacity-50" 
+          className="w-full h-full object-cover opacity-50"
           autoPlay
           loop
           muted
-          />
+        />
       </div>
-      <div className="relative flex items-center w-full h-full z-10">
+      <div className="relative flex items-center justify-center w-full h-full z-10">
         <div className="max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8 text-center">
-          <h1 className="text-4xl font-extrabold tracking-tight text-green-700 sm:text-5xl md:text-6xl">
+          <div className="w-[65%] h-auto m-auto">
+            <img src="/logo2.png" className="w-full h-auto" alt="logo" />
+          </div>
+
+          <h1 className="text-4xl font-extrabold tracking-tight text-green-600 sm:text-5xl md:text-6xl">
             Bienvenido a <span className="text-white">Green Places</span>
           </h1>
           <p className="mt-6 max-w-2xl mx-auto text-xl text-white">
@@ -23,7 +26,7 @@ const Hero = () => {
           <div className="mt-10 flex justify-center gap-4">
             <a
               href="#educacion"
-              className="px-8 py-3 border border-white text-base font-medium rounded-md text-white bg-green-700 hover:bg-green-900 md:text-lg transition duration-500"
+              className="px-8 py-3 border border-white text-base font-medium rounded-md text-white bg-green-800 hover:bg-lime-600 md:text-lg transition duration-500"
             >
               Aprende con nosotros
             </a>
